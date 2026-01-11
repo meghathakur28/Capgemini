30c10ecf94e0423395a77f22fd51b542a498e6d7
Megha Thakur <118892451+meghathakur28@users.noreply.github.com>
2026-01-11 23:19:50 +0530

    Add files via upload

---
+++ WinSerializeDemo/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace WinSerializeDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.EmployeeID = new System.Windows.Forms.Label();
+            this.Name = new System.Windows.Forms.Label();
+            this.txtEmployeeID = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtSalary = new System.Windows.Forms.TextBox();
+            this.Salary = new System.Windows.Forms.Label();
+            this.btnBinSerialize = new System.Windows.Forms.Button();
+            this.btnBinDeSeriallize = new System.Windows.Forms.Button();
+            this.btnXMLSerialize = new System.Windows.Forms.Button();
+            this.btnXMLDeSerialize = new System.Windows.Forms.Button();
+            this.btnSOAPSerialize = new System.Windows.Forms.Button();
+            this.btnSOAPDeSerialize = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // EmployeeID
+            // 
+            this.EmployeeID.AutoSize = true;
+            this.EmployeeID.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EmployeeID.Location = new System.Drawing.Point(228, 140);
+            this.EmployeeID.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.EmployeeID.Name = "EmployeeID";
+            this.EmployeeID.Size = new System.Drawing.Size(92, 16);
+            this.EmployeeID.TabIndex = 0;
+            this.EmployeeID.Text = "EmployeeID";
+            this.EmployeeID.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Name
+            // 
+            this.Name.AutoSize = true;
+            this.Name.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name.Location = new System.Drawing.Point(228, 182);
+            this.Name.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Name.Name = "Name";
+            this.Name.Size = new System.Drawing.Size(48, 16);
+            this.Name.TabIndex = 1;
+            this.Name.Text = "Name";
+            this.Name.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtEmployeeID
+            // 
+            this.txtEmployeeID.Location = new System.Drawing.Point(336, 137);
+            this.txtEmployeeID.Margin = new System.Windows.Forms.Padding(4);
+            this.txtEmployeeID.Name = "txtEmployeeID";
+            this.txtEmployeeID.Size = new System.Drawing.Size(148, 22);
+            this.txtEmployeeID.TabIndex = 2;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(336, 182);
+            this.txtName.Margin = new System.Windows.Forms.Padding(4);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(148, 22);
+            this.txtName.TabIndex = 3;
+            this.txtName.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // txtSalary
+            // 
+            this.txtSalary.Location = new System.Drawing.Point(336, 225);
+            this.txtSalary.Margin = new System.Windows.Forms.Padding(4);
+            this.txtSalary.Name = "txtSalary";
+            this.txtSalary.Size = new System.Drawing.Size(148, 22);
+            this.txtSalary.TabIndex = 6;
+            // 
+            // Salary
+            // 
+            this.Salary.AutoSize = true;
+            this.Salary.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Salary.Location = new System.Drawing.Point(228, 229);
+            this.Salary.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Salary.Name = "Salary";
+            this.Salary.Size = new System.Drawing.Size(52, 16);
+            this.Salary.TabIndex = 4;
+            this.Salary.Text = "Salary";
+            this.Salary.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // btnBinSerialize
+            // 
+            this.btnBinSerialize.Location = new System.Drawing.Point(91, 294);
+            this.btnBinSerialize.Name = "btnBinSerialize";
+            this.btnBinSerialize.Size = new System.Drawing.Size(164, 23);
+            this.btnBinSerialize.TabIndex = 7;
+            this.btnBinSerialize.Text = "Bin Serialize";
+            this.btnBinSerialize.UseVisualStyleBackColor = true;
+            this.btnBinSerialize.Click += new System.EventHandler(this.btnBinSerialize_Click);
+            // 
+            // btnBinDeSeriallize
+            // 
+            this.btnBinDeSeriallize.Location = new System.Drawing.Point(91, 345);
+            this.btnBinDeSeriallize.Name = "btnBinDeSeriallize";
+            this.btnBinDeSeriallize.Size = new System.Drawing.Size(164, 23);
+            this.btnBinDeSeriallize.TabIndex = 8;
+            this.btnBinDeSeriallize.Text = "Bin DeSerialize";
+            this.btnBinDeSeriallize.UseVisualStyleBackColor = true;
+            this.btnBinDeSeriallize.Click += new System.EventHandler(this.btnBinDeSeriallize_Click);
+            // 
+            // btnXMLSerialize
+            // 
+            this.btnXMLSerialize.Location = new System.Drawing.Point(290, 294);
+            this.btnXMLSerialize.Name = "btnXMLSerialize";
+            this.btnXMLSerialize.Size = new System.Drawing.Size(151, 23);
+            this.btnXMLSerialize.TabIndex = 9;
+            this.btnXMLSerialize.Text = "XML Serialize";
+            this.btnXMLSerialize.UseVisualStyleBackColor = true;
+            this.btnXMLSerialize.Click += new System.EventHandler(this.btnXMLSerialize_Click);
+            // 
+            // btnXMLDeSerialize
+            // 
+            this.btnXMLDeSerialize.Location = new System.Drawing.Point(290, 345);
+            this.btnXMLDeSerialize.Name = "btnXMLDeSerialize";
+            this.btnXMLDeSerialize.Size = new System.Drawing.Size(151, 23);
+            this.btnXMLDeSerialize.TabIndex = 10;
+            this.btnXMLDeSerialize.Text = "XML DeSerialize";
+            this.btnXMLDeSerialize.UseVisualStyleBackColor = true;
+            this.btnXMLDeSerialize.Click += new System.EventHandler(this.btnXMLDeSerialize_Click);
+            // 
+            // btnSOAPSerialize
+            // 
+            this.btnSOAPSerialize.Location = new System.Drawing.Point(508, 294);
+            this.btnSOAPSerialize.Name = "btnSOAPSerialize";
+            this.btnSOAPSerialize.Size = new System.Drawing.Size(169, 23);
+            this.btnSOAPSerialize.TabIndex = 11;
+            this.btnSOAPSerialize.Text = "SOAP Serialize";
+            this.btnSOAPSerialize.UseVisualStyleBackColor = true;
+            this.btnSOAPSerialize.Click += new System.EventHandler(this.btnSOAPSerialize_Click);
+            // 
+            // btnSOAPDeSerialize
+            // 
+            this.btnSOAPDeSerialize.Location = new System.Drawing.Point(508, 345);
+            this.btnSOAPDeSerialize.Name = "btnSOAPDeSerialize";
+            this.btnSOAPDeSerialize.Size = new System.Drawing.Size(169, 23);
+            this.btnSOAPDeSerialize.TabIndex = 12;
+            this.btnSOAPDeSerialize.Text = "SOAP De Serialize";
+            this.btnSOAPDeSerialize.UseVisualStyleBackColor = true;
+            this.btnSOAPDeSerialize.Click += new System.EventHandler(this.btnSOAPDeSeriallize_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 554);
+            this.Controls.Add(this.btnSOAPDeSerialize);
+            this.Controls.Add(this.btnSOAPSerialize);
+            this.Controls.Add(this.btnXMLDeSerialize);
+            this.Controls.Add(this.btnXMLSerialize);
+            this.Controls.Add(this.btnBinDeSeriallize);
+            this.Controls.Add(this.btnBinSerialize);
+            this.Controls.Add(this.txtSalary);
+            this.Controls.Add(this.Salary);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtEmployeeID);
+            this.Controls.Add(this.Name);
+            this.Controls.Add(this.EmployeeID);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            //this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label EmployeeID;
+        private System.Windows.Forms.Label Name;
+        private System.Windows.Forms.TextBox txtEmployeeID;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtSalary;
+        private System.Windows.Forms.Label Salary;
+        private System.Windows.Forms.Button btnBinDeSeriallize;
+        private System.Windows.Forms.Button btnXMLSerialize;
+        private System.Windows.Forms.Button btnXMLDeSerialize;
+        private System.Windows.Forms.Button btnSOAPSerialize;
+        private System.Windows.Forms.Button btnSOAPDeSerialize;
+        private System.Windows.Forms.Button btnBinSerialize;
+    }
+}
+
---
+++ WinSerializeDemo/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//FOr binary serialization
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+//For XML 
+using System.Xml.Serialization;
+
+//For SOAP Serialization
+using System.Runtime.Serialization.Formatters.Soap;
+
+namespace WinSerializeDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void ClearAllTextBoxes()
+        {
+            foreach (Control item in this.Controls)
+            {
+                if (item.GetType() == typeof(TextBox))
+                {
+                    TextBox txtBox = (TextBox)item;
+                    txtBox.Clear();
+                }
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBinSerialize_Click(object sender, EventArgs e)
+        {
+            Employee emp1 = new Employee();
+            emp1.ID = Convert.ToInt32(txtEmployeeID.Text);
+            emp1.name = txtName.Text;
+            emp1.Salary = Convert.ToInt32(txtSalary.Text);
+
+            //Binary Serialization
+
+            FileStream fs = new FileStream(@"D:\.net\BinSearialize.bin", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            BinaryFormatter bf = new BinaryFormatter();
+
+            //serialize in fs file from emp1 object
+            bf.Serialize(fs, emp1);
+
+            //clear all the text box values
+            ClearAllTextBoxes();
+            fs.Close();
+
+            MessageBox.Show("Record Added Successfully");
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBinDeSeriallize_Click(object sender, EventArgs e)
+        {
+            //Binary DeSerialization
+
+            FileStream fs = new FileStream(@"D:\.net\BinSearialize.bin", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            BinaryFormatter bf = new BinaryFormatter();
+
+            //Deserialize from fs file to emp1 object
+            Employee emp1 = (Employee)bf.Deserialize(fs);
+            
+            txtEmployeeID.Text = emp1.ID.ToString();
+            txtName.Text = emp1.name;
+            txtSalary.Text = emp1.Salary.ToString();
+        }
+
+        private void btnXMLSerialize_Click(object sender, EventArgs e)
+        {
+
+            Employee emp1 = new Employee();
+            emp1.ID = Convert.ToInt32(txtEmployeeID.Text);
+            emp1.name = txtName.Text;
+            emp1.Salary = Convert.ToInt32(txtSalary.Text);
+
+            //XML Serialization
+
+            FileStream fs = new FileStream(@"D:\.net\XMLSearialize.xml", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            XmlSerializer xm = new XmlSerializer(typeof(Employee)); // xml is type based so we have to mention.
+
+            //serialize in fs file from emp1 object
+            xm.Serialize(fs, emp1);
+
+            //clear all the text box values
+            ClearAllTextBoxes();
+            fs.Close();
+            MessageBox.Show("Record Added Successfully");
+        }
+
+        private void btnXMLDeSerialize_Click(object sender, EventArgs e)
+        {
+            //xml DeSerialization
+
+            FileStream fs = new FileStream(@"D:\.net\XMLSearialize.xml", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            XmlSerializer xm = new XmlSerializer(typeof(Employee)); // xml is type based so we have to mention.
+
+            //Deserialize from fs file to emp1 object
+            Employee emp1 = (Employee)xm.Deserialize(fs);
+
+            txtEmployeeID.Text = emp1.ID.ToString();
+            txtName.Text = emp1.name;
+            txtSalary.Text = emp1.Salary.ToString();
+        }
+
+        private void btnSOAPSerialize_Click(object sender, EventArgs e)
+        {
+            Employee emp1 = new Employee();
+            emp1.ID = Convert.ToInt32(txtEmployeeID.Text);
+            emp1.name = txtName.Text;
+            emp1.Salary = Convert.ToInt32(txtSalary.Text);
+
+            //SOAP Serialization
+
+            FileStream fs = new FileStream(@"D:\.net\SOAPSearialize.xml", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            SoapFormatter bf = new SoapFormatter();
+
+            //serialize in fs file from emp1 object
+            bf.Serialize(fs, emp1);
+
+            //clear all the text box values
+            ClearAllTextBoxes();
+            fs.Close();
+
+            MessageBox.Show("Record Added Successfully");
+        }
+
+        private void btnSOAPDeSeriallize_Click(object sender, EventArgs e)
+        {
+            //SOAP DeSerialization
+
+            FileStream fs = new FileStream(@"D:\.net\SOAPSearialize.xml", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            SoapFormatter bf = new SoapFormatter();
+
+            //Deserialize from fs file to emp1 object
+            Employee emp1 = (Employee)bf.Deserialize(fs);
+
+            txtEmployeeID.Text = emp1.ID.ToString();
+            txtName.Text = emp1.name;
+            txtSalary.Text = emp1.Salary.ToString();
+        }
+    }
+}
