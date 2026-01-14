f70cb7134153a883682e24258faa800cdf605e4c
Megha Thakur <118892451+meghathakur28@users.noreply.github.com>
2026-01-14 22:50:33 +0530

    Add files via upload

---
+++ Event-DrivenPhoneCallSubscription/PhoneCall.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Event_DrivenPhoneCallSubscription
+{
+    public delegate void Notify();
+
+    
+    public class PhoneCall
+    {
+        public string Message 
+        { get; private set; }
+        public event  Notify PhoneCallEvent;
+        
+
+        private void OnSubscribe()
+        {
+            Message = "Subscribes to Call";
+        }
+
+        private void OnUnsubscribe()
+        {
+            Message = "UnSubscribes to Call";
+        }
+
+        public void MakeAPhoneCall(bool notify)
+        {
+            if(notify)
+            {
+                PhoneCallEvent = OnSubscribe;
+            }
+            else
+            {
+                PhoneCallEvent = OnUnsubscribe;
+            }
+
+            PhoneCallEvent();
+        }
+
+    }
+}
---
+++ Event-DrivenPhoneCallSubscription/Program.cs
@@ -0,0 +1,10 @@
+﻿
+using System;
+using Event_DrivenPhoneCallSubscription;
+
+PhoneCall pc = new PhoneCall();
+pc.MakeAPhoneCall(true);
+Console.WriteLine(pc.Message);
+pc.MakeAPhoneCall(false);
+Console.WriteLine(pc.Message);
+
