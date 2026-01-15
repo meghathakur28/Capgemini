b36613c4f862ff1338188a67283f0a86a337841a
Megha Thakur <118892451+meghathakur28@users.noreply.github.com>
2026-01-16 00:40:53 +0530

    Add files via upload

---
+++ PrepareBill_3/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrepareBill_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var commodities = new List<Commodity>()
+            {
+                new Commodity(CommodityCategory.Furniture,"Bed",2,50000),
+                new Commodity(CommodityCategory.Grocery,"Flour",5,80),
+                new Commodity(CommodityCategory.Service,"Insurance",8,8500)
+            };
+            var prepareBill = new PrepareBill();
+            prepareBill.SetTaxRates(CommodityCategory.Furniture, 18);
+            prepareBill.SetTaxRates(CommodityCategory.Grocery, 5);
+            prepareBill.SetTaxRates(CommodityCategory.Service, 12);
+
+            var billAmount = prepareBill.CalculateBillAmount(commodities);
+            Console.WriteLine($"{billAmount}");
+        }
+    }
+    public enum CommodityCategory
+    {
+        Furniture,
+        Grocery,
+        Service
+    }
+    class Commodity
+    {
+        public CommodityCategory category {  get; set; }
+
+        public string CommodityName {  get; set; }
+
+        public int CommodityQuantity {  get; set; }
+
+        public double CommodityPrice { get; set; }
+        public Commodity(CommodityCategory category, string commodityName,int commodityQuantity,double commodityPrice)
+        {
+            this.category = category;
+            this.CommodityName = commodityName;
+            this.CommodityPrice = commodityPrice;
+            this.CommodityQuantity = commodityQuantity;
+        }
+
+
+    }
+    class PrepareBill
+    {
+        private readonly IDictionary<CommodityCategory, double> _taxRates;
+
+        public PrepareBill()
+        {
+            _taxRates = new Dictionary<CommodityCategory, double>();
+        }
+
+        public void SetTaxRates(CommodityCategory category,double taxRate)
+        {
+            if(!_taxRates.Keys.Contains(category)) 
+                {
+                    _taxRates.Add(category, taxRate);
+                }
+        }
+        public double CalculateBillAmount(IList<Commodity> items)
+        {
+            double total = 0;
+            foreach (var item in items)
+            {
+               if(_taxRates.Keys.Contains(item.category))
+                {
+                    total += (item.CommodityQuantity *( item.CommodityPrice + (_taxRates[item.category]*item.CommodityPrice)/100));
+                }
+                else
+                {
+                    throw new ArgumentException();
+                }
+            }
+            return total;
+        }
+
+    }
+}
---
+++ PrepareBill_3/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PrepareBill_3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PrepareBill_3")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("453ae24b-1a48-42e4-b428-fba2ee05bc50")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
