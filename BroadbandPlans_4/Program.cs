b6af01706234db3e75f69443ec2dd006b9ed9a7d
Megha Thakur <118892451+meghathakur28@users.noreply.github.com>
2026-01-16 00:41:30 +0530

    Add files via upload

---
+++ BroadbandPlans_4/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BroadbandPlans_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var plans = new List<IBroadbandPlan>
+            {
+                new Black(true,50),
+                new Black(false,10),
+                new Gold(true,30),
+                new Black(true,20),
+                new Gold(false,20)
+            };
+
+            var subscriptionPlans = new SubscribePlan(plans);
+            var result = subscriptionPlans.GetSubscriptionPlan();
+
+            foreach (var item in result)
+            {
+                Console.WriteLine($"{item.Item1},{item.Item2}");
+            }
+
+        }
+    }
+    interface IBroadbandPlan
+    {
+        int GetBroadbandPlanAmount();
+    }
+    class Black : IBroadbandPlan
+    {
+        private readonly bool _isSubscriptionValid;
+
+        private readonly int _discountpercentage;
+
+        private const int PlanAmount = 3000;
+
+        public Black(bool isSubscriptionValid, int discountpercentage)
+        {
+            this._isSubscriptionValid = isSubscriptionValid;
+            if (discountpercentage < 0 || discountpercentage > 50)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            this._discountpercentage = discountpercentage;
+        }
+
+        public int GetBroadbandPlanAmount()
+        {
+            if(_isSubscriptionValid)
+            {
+                return PlanAmount - (_discountpercentage * PlanAmount / 100);
+            }
+            return PlanAmount;
+        }
+
+    }
+
+    class Gold:IBroadbandPlan
+    {
+        private readonly bool _isSubscriptionValid;
+
+        private readonly int _discountpercentage;
+
+        private const int PlanAmount = 1500;
+
+        public Gold(bool isSubscriptionValid, int discountpercentage)
+        {
+            this._isSubscriptionValid = isSubscriptionValid;
+            if (discountpercentage < 0 || discountpercentage > 30)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            this._discountpercentage = discountpercentage;
+        }
+        public int GetBroadbandPlanAmount()
+        {
+            if (_isSubscriptionValid)
+            {
+                return PlanAmount - (_discountpercentage * PlanAmount / 100);
+            }
+            return PlanAmount;
+        }
+
+    }
+
+    class SubscribePlan
+    {
+        private readonly IList<IBroadbandPlan> _broadbandPlans;
+
+        public SubscribePlan(IList<IBroadbandPlan> broadbandPlans)
+        {
+            _broadbandPlans = broadbandPlans;
+        }
+
+        public IList<Tuple<string, int>> GetSubscriptionPlan()
+        {
+            IList<Tuple<string, int>> plans = new List<Tuple<string, int>>();
+            if (_broadbandPlans == null)
+            {
+                throw new ArgumentException();
+            }
+            foreach (var plan in _broadbandPlans)
+            {
+                plans.Add(new Tuple<string, int>(plan.GetType().Name, plan.GetBroadbandPlanAmount()));
+            }
+            return plans;
+        }
+    }
+}
---
+++ BroadbandPlans_4/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BroadbandPlans_4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BroadbandPlans_4")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d9423ee5-0737-4264-b594-8b0b216a784f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
