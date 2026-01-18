7e36f179d36665eaa2fa99b0e34ca17348328210
Megha Thakur <118892451+meghathakur28@users.noreply.github.com>
2026-01-18 22:08:52 +0530

    Add files via upload

---
+++ ConstructionEstimate/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructionEstimate
+{
+    public class Program
+    {
+
+        public EstimateDetails ValidateConstructionEstimate(float ConstructionArea , float siteArea)
+        {
+            EstimateDetails details = new EstimateDetails();
+            if(ConstructionArea <= siteArea)
+            {
+                details.ConstructionArea = siteArea; 
+                details.SiteArea = siteArea;
+                return details;
+            }
+            throw new ConstructionEstimateException("Sorry your Construction Estimate is not approved");
+        }
+        public static void Main(string[] args)
+        {
+            Program p = new Program();
+            Console.WriteLine("Enter the construction area: ");
+            float constructionArea = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the SiteArea: ");
+            float siteArea = float.Parse(Console.ReadLine());
+
+            try
+            {
+                p.ValidateConstructionEstimate(constructionArea, siteArea);
+                Console.WriteLine("Approved");
+            }
+            catch(ConstructionEstimateException e)
+            {
+                Console.WriteLine(e.Message); 
+            }
+
+
+        }
+    }
+    public class EstimateDetails
+    {
+        public float ConstructionArea {  get; set; }
+        public float SiteArea { get; set; }
+    }
+    public class ConstructionEstimateException:Exception
+    {
+        public ConstructionEstimateException() { }
+
+        public ConstructionEstimateException(string message) : base(message) { }
+
+
+
+    }
+}
---
+++ ConstructionEstimate/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ConstructionEstimate")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConstructionEstimate")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1c950ee2-fcd7-46b9-9c7c-23f377d62b22")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
