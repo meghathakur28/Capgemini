8400acb33e28457c7b27d0aacf3b0650475ea0be
Megha Thakur <118892451+meghathakur28@users.noreply.github.com>
2026-01-20 01:30:00 +0530

    Add files via upload

---
+++ TopBrains/CustomSorting_20/CustomSorting_20/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomSorting_20
+{
+    public class Student
+    {
+        public string Name {get; set; }
+        public int Age { get; set; }
+
+        public double Marks {  get; set; }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> student = new List<Student>();
+            student.Add(new Student { Name = "Megha", Age = 21, Marks = 81.4 });
+            student.Add(new Student { Name = "Abhinav", Age = 20, Marks = 90.23});
+            student.Add(new Student { Name = "Parth", Age = 22, Marks = 81.10 });
+            student.Add(new Student { Name = "Nisha Agarwal", Age = 23, Marks = 81.98 });
+            student.Add(new Student { Name = "Anshika", Age = 22, Marks = 68.56 });
+
+            var SortedList = student.OrderByDescending(x => x.Marks).ThenBy(x => x.Age).ToList();
+
+            Console.WriteLine("The Sorted List: ");
+            foreach(var i in SortedList)
+            {
+                Console.WriteLine($"Name: {i.Name}, Marks: {i.Marks}, Age: {i.Age} ");
+            }
+        }
+
+
+    }
+}
