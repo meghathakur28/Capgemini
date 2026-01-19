8f0575c1e0565e86c94b125492d6220fe0f893c2
Megha Thakur <118892451+meghathakur28@users.noreply.github.com>
2026-01-20 01:13:33 +0530

    Add files via upload

---
+++ TopBrains/Parsing_16/Parsing_16/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parsing_16
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] tokens = { "21", "88", "90" };
+            int sum = 0;
+            foreach (string token in tokens)
+            {
+                int t = Int32.Parse(token);
+                sum += t;
+            }
+            Console.WriteLine(sum);
+        }
+
+    }
+}
