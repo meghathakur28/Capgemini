a348f453684d939c7ec4b37158063be3926a73f0
Megha Thakur <118892451+meghathakur28@users.noreply.github.com>
2026-01-19 23:35:17 +0530

    Add files via upload

---
+++ TopBrains/SumOfPositiveIntegers_12/SumOfPositiveIntegers_12/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfPositiveIntegers_12
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the n : number of elements");
+            int n = Int32.Parse(Console.ReadLine());
+
+            int[] arr = new int[n];
+
+            for(int i=0;i<n;i++)
+            {
+                arr[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            int sum = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == 0) break;
+                if (arr[i] < 0) continue;
+                sum += arr[i];
+            }
+            Console.WriteLine("The sum is : "+ sum);
+        }
+    }
+}
