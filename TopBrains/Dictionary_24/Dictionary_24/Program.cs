7aaba92466884ad0a0d378be640fcf50e389117a
Megha Thakur <118892451+meghathakur28@users.noreply.github.com>
2026-01-20 00:32:07 +0530

    Add files via upload

---
+++ TopBrains/Dictionary_24/Dictionary_24/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Dictionary<int,double> employees = new Dictionary<int,double>();
+        List<int> employeeids = new List<int>() { 1,4,5};
+
+        for(int i=0;i<employeeids.Count;i++)
+        {
+            Console.WriteLine("Enter the salary for the employee with employee id:"+employeeids[i]);
+            employees[employeeids[i]] = double.Parse(Console.ReadLine());
+        }
+
+        var salary = employees.Select(x => x.Value).Sum();
+        Console.WriteLine("The sum of salary is: " + salary);
+
+
+
+    }
+}
