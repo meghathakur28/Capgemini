8f8fcfe042839e3036533579c66882a1e9f22666
Megha Thakur <118892451+meghathakur28@users.noreply.github.com>
2026-01-20 01:07:20 +0530

    Add files via upload

---
+++ TopBrains/Conversion_19/Conversion_19/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversion_19
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the height in feet ");
+            int foot = Int32.Parse(Console.ReadLine());
+
+            double cm = (double) foot * 30.48;
+
+            Console.WriteLine(Math.Round(cm, 2));
+
+
+        }
+    }
+}
