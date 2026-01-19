a41fde242d0ce479047704136839e70f3321bb2e
Megha Thakur <118892451+meghathakur28@users.noreply.github.com>
2026-01-19 15:07:02 +0530

    Add files via upload

---
+++ TopBrains/DisplayHeight_7/DisplayHeight_7/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisplayHeight_7
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the height in cms");
+            int height = Int32.Parse(Console.ReadLine());
+
+            if(height < 150)
+            {
+                Console.WriteLine("Short");
+            }
+            else if(height >= 150&& height<180)
+            {
+                Console.WriteLine("Average");
+            }
+            else if(height >=180)
+            {
+                Console.WriteLine("Tall");
+            }
+        }
+    }
+}
