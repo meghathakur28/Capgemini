3fa05b0be2c5a5bde3072276f397c0276527a565
Megha Thakur <118892451+meghathakur28@users.noreply.github.com>
2026-01-19 23:25:31 +0530

    Add files via upload

---
+++ TopBrains/InventoryNameCleanup_11/InventoryNameCleanup_11/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryNameCleanup_11
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the string: ");
+            string word = Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if(i==0 || word[i] != word[i-1])
+                {
+                    sb.Append(word[i]);
+                }
+            }
+
+            string ans = sb.ToString();
+
+            string ansTitleCase = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(ans.Trim().ToLower());
+            Console.WriteLine(ansTitleCase);
+
+        }
+    }
+}
