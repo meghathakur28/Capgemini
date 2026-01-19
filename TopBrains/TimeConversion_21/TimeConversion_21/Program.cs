645d32061056d17bd7c71fe687cfaab0f489da0b
Megha Thakur <118892451+meghathakur28@users.noreply.github.com>
2026-01-20 01:42:42 +0530

    Add files via upload

---
+++ TopBrains/TimeConversion_21/TimeConversion_21/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeConversion_21
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the seconds: ");
+            int totalSeconds = Int32.Parse(Console.ReadLine());
+
+            int min = totalSeconds / 60;
+            int sec = totalSeconds % 60;
+            if (sec < 10)
+            {
+                Console.WriteLine($"The time is: {min}:0{sec}");
+                return;
+            }
+            Console.WriteLine($"The time is: {min}:{sec}");
+        }
+    }
+}
