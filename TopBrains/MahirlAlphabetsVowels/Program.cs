5bcb2465d681278477d00f884fb9caa5e8d6293e
Megha Thakur <118892451+meghathakur28@users.noreply.github.com>
2026-01-19 10:27:06 +0530

    Add files via upload

---
+++ TopBrains/MahirlAlphabetsVowels/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MahirlAlphabetsVowels
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first word ");
+            string w1 = Console.ReadLine().ToLower();
+             
+
+            Console.WriteLine("Enter the second word");
+            string w2 = Console.ReadLine().ToLower();
+
+            string consonants =String.Empty;
+
+            foreach (var i in w1)
+            {
+                if (i == 'a' || i=='e'|| i =='i' || i== 'o'|| i=='u')
+                {
+                    if (w2.Contains(i))
+                    {
+                        consonants += i;
+                    }
+                }
+            }
+
+            StringBuilder ans = new StringBuilder();
+            for(int i= 1; i < consonants.Length;i++)
+            {
+                if (i==0 || consonants[i] != consonants[i-1] )
+                {
+                    ans.Append(consonants[i]);
+                }
+            }
+
+            Console.WriteLine(ans);
+
+
+
+
+        }
+    }
+}
