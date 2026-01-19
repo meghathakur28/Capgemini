59e687b7ecbe4766b1f8e761884c2aa91744dc1f
Megha Thakur <118892451+meghathakur28@users.noreply.github.com>
2026-01-19 15:38:51 +0530

    Add files via upload

---
+++ TopBrains/BankTransaction_9/BankTransaction_9/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankTransaction_9
+{
+    public  class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the balance: ");
+            int balance = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enterthe number of transaction: ");
+            int n = Int32.Parse(Console.ReadLine());
+
+            int[] transaction = new int[n];
+
+            for(int i=0;i<n;i++)
+            {
+                transaction[i] = Int32.Parse(Console.ReadLine());
+                if (transaction[i] > 0)
+                {
+                    balance += transaction[i];
+                }
+                else
+                {
+                    if (balance >= transaction[i])
+                    {
+                        balance += transaction[i];
+                    }
+                }
+            }
+
+            Console.WriteLine("Final balance: " + balance);
+        }
+    }
+}
