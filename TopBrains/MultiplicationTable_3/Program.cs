3089d42511f28ef08b93b2f3149b11dcd981ce83
Megha Thakur <118892451+meghathakur28@users.noreply.github.com>
2026-01-19 11:48:20 +0530

    Add files via upload

---
+++ TopBrains/MultiplicationTable_3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplicationTable_3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the n: ");
+            int n = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the number (upto table) : ");
+            int upto = Int32.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= upto; i++)
+            {
+                Console.WriteLine(n + " * " + i + " = " + n * i);
+            }
+        }
+    }
+}
