f0dbc53f89605c822de86bb859cee64164eea965
Megha Thakur <118892451+meghathakur28@users.noreply.github.com>
2026-01-20 14:58:02 +0530

    Add files via upload

---
+++ TopBrains/ObjectArray_23/ObjectArray_23/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectArray_23
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the value n (no of elements in the array: ");
+            int n = Int32.Parse(Console.ReadLine());
+            Object[] arr = new Object[n];
+
+            for(int i=0;i<n;i++)
+            {
+                arr[i] = Console.ReadLine();
+            }
+            int sum = 0;
+            foreach (var i in arr)
+            {
+                string s = i.ToString();
+                if(Int32.TryParse(s,out var value))
+                {
+                    sum += value;
+                }
+            }
+
+            Console.WriteLine($"The sum is {sum}");
+
+
+        }
+    }
+}
