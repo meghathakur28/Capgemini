f9e3838b328a3d349753d43f1f46f5328d92ec67
Megha Thakur <118892451+meghathakur28@users.noreply.github.com>
2026-01-20 15:52:12 +0530

    Add files via upload

---
+++ TopBrains/MidPointRounding_25/MidPointRounding_25/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidPointRounding_25
+{
+    public class Program
+    {
+        public static double Area(double radius) => Math.Round(3.14* radius * radius,2);
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the radius: ");
+            double radius = double.Parse(Console.ReadLine());
+            System.Console.WriteLine("The radius is: " + Area(radius));
+
+        }
+    }
+}
---
+++ TopBrains/MidPointRounding_25/MidPointRounding_25/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MidPointRounding_25")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MidPointRounding_25")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c3e46145-7787-4224-985a-059a7fe19dfa")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
