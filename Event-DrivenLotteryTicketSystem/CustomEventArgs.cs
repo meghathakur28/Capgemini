9a22780c4d54d65eb97c2390cb0ca575b55411ef
Megha Thakur <118892451+meghathakur28@users.noreply.github.com>
2026-01-21 15:49:05 +0530

    Add files via upload

---
+++ Event-DrivenLotteryTicketSystem/CustomEventArgs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Event_DrivenLotteryTicketSystem
+{
+    public class CustomEventArgs:EventArgs
+    {
+        public int Quantity { get; set; }
+
+    }
+}
---
+++ Event-DrivenLotteryTicketSystem/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Event_DrivenLotteryTicketSystem;
+
+class Program
+{
+        static void Main(string[] args)
+        {
+            /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+            var lottery = new Lottery(new CustomEventArgs { Quantity = 10 });
+            var winners = lottery.LotteryWinners();
+            var index = 1;
+            foreach (var winner in winners)
+                Console.WriteLine($"{index++}.{winner}");
+
+        }
+}
